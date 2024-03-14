a061c27a6343a9dc5b73348139e6c018759e30cb
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-03-14 11:37:10 +0900

    adding 'try' and 'except' errors

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ import pandas
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # Crea un diccionario a partir de los datos del CSV, asignando a cada letra su código fonético correspondiente
-data_dictionary = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
+phonetic_dictionary = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
+# print(phonetic_dictionary)
 
-
-# print(data_dictionary)
 
 # TODO 2. El usuario ingresa una palabra y el programa debe deletrearla correctamente
-sentence = input("Ingrese una palabra que desee ver deletreada utilizando el alfabeto de la OTAN: ")
 
-# Convierte la entrada a mayúsculas para garantizar consistencia
-capital_letters = sentence.upper()
 
-# Crea una lista de códigos fonéticos correspondientes a cada letra en la palabra ingresada
-lista_de_palabras = [data_dictionary[letter] for letter in capital_letters if letter in data_dictionary]
+def calcular_phonetic_sound():
+    sentence = input("Ingrese una palabra que desee ver deletreada utilizando el alfabeto de la OTAN: ")
+    # Convierte la entrada a mayúsculas para garantizar consistencia
+    word = sentence.upper()
 
-# Imprime la lista de códigos fonéticos
-print(lista_de_palabras)
+    # Crea una lista de códigos fonéticos correspondientes a cada letra en la palabra ingresada
+    try:
+        lista_de_palabras = [phonetic_dictionary[letter] for letter in word ]
+        # Imprime la lista de códigos fonéticos
+        print(lista_de_palabras)
+    except KeyError:
+        print('Sorry, only letters in the alfabet please')
+        # sente ce = input("Ingrese una palabra que desee ver deletreada utilizando el alfabeto de la OTAN: ")
+        calcular_phonetic_sound()
+     
+calcular_phonetic_sound()        
